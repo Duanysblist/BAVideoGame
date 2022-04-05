2ab43da04d716af956d1648032217aa7bf794a33
sgsmith <sgsmith@email.wm.edu>
2022-04-04 22:42:39 -0400

    moved attributes and include statements to header for renderer

--- include/bionic_apocalypse_renderer.h
+++ include/bionic_apocalypse_renderer.h
@@ -1,13 +1,35 @@
-#include <string>
-//#include "bionic_apocalypse_enemy.h"
-#include "bionic_apocalypse_constants.h"
-#include "bionic_apocalypse_player.h"
-
 
 #ifndef BIONIC_APOCALYPSE_RENDERER_H
 #define BIONIC_APOCALYPSE_RENDERER_H
 
+#include <string>
+#include "bionic_apocalypse_constants.h"
+#include "bionic_apocalypse_player.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include <SDL2_gfxPrimitives.h>
+
 class Renderer {
+    private:
+        //Window and image variables
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        SDL_Surface* image;
+        SDL_Texture* player_texture;
+        SDL_Texture* enemy;
+        SDL_Rect player_rect; 
+        SDL_Rect enemy_rect = { 0, 0, ENEMY_WIDTH, ENEMY_HEIGHT };
+        TTF_Font* font;
+        SDL_Texture* texture;
+        SDL_Color color = { 255, 255, 255 };
+        int BAR_LENGTH = 200;
+        int BAR_START = 30;
+
     public:
         void csci437_error(const std::string& msg) const;
 
