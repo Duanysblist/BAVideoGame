f3996d7fbccc6f5cd384f493f7bac6c2613c3859
sgsmith <sgsmith@email.wm.edu>
2022-04-11 14:50:08 -0400

    commented out cout statements

--- src/bionic_apocalypse_battlelogic.cpp
+++ src/bionic_apocalypse_battlelogic.cpp
@@ -16,7 +16,7 @@ Enemy mainEnemy;
 bool active = false;
 
 void startNewBattle(Enemy enemy) {
-	std::cout << "battle and bad guy initiated" << std::endl; 
+	// std::cout << "battle and bad guy initiated" << std::endl; 
 	mainEnemy = enemy;
 	active = true;
 }
@@ -25,8 +25,8 @@ void setAttackingTrue(Player player) {
 	if (player.getPlayerHealth() > 0 && mainEnemy.getHealth() > 0) {
 		mainEnemy.changeHealth(-9);
 		player.changePlayerHealth(mainEnemy.randomAttack());
-		std::cout << "Player HP: " << player.getPlayerHealth() << ", Enemy HP: " << mainEnemy.getHealth() << std::endl;
-		std::cout << "Attacking" << "\n";
+		// std::cout << "Player HP: " << player.getPlayerHealth() << ", Enemy HP: " << mainEnemy.getHealth() << std::endl;
+		// std::cout << "Attacking" << "\n";
 	}
 	else {
 		active = false;
