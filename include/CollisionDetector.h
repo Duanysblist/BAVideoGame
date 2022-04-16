790f75655085208d6dde8ce8b4d2f0bce854a9d6
sgsmith <sgsmith@email.wm.edu>
2022-04-15 20:49:52 -0400

    updated skeleton for collision detection

--- include/CollisionDetector.h
+++ include/CollisionDetector.h
@@ -3,11 +3,36 @@
 
 #include "bionic_apocalypse_player.h"
 #include "Scene.h"
+#include "bionic_apocalypse_constants.h"
+
+
+/** 
+    collision detector code only works as long as 
+    each screen "block" width is at least half of the player's width
+    and each screen "block" height is at least half of the player's height
+**/
 
 class CollisionDetector {
 
+
+private:
+    int playerX;
+    int playerY;
+
+    int playerRight;
+    int playerBottom;
+
+    int blockHeight;
+    int blockWidth;
+
+
+    void playerUpperLeftResourceCollision(const Player &player, const Scene &scene) const;
+    void playerUpperRightResourceCollision(const Player &player, const Scene &scene) const;
+    void playerBottomLeftResourceCollision(const Player &player, const Scene &scene) const;
+    void playerBottomRightResourceCollision(const Player &player, const Scene &scene) const;
+
 public:
-    bool playerResourceCollision(const Player &player, const Scene &scene) const;
+    void playerResourceCollision(const Player &player, const Scene &scene);
 
 };
 
--- src/CollisionDetector.cpp
+++ src/CollisionDetector.cpp
@@ -1,5 +1,25 @@
 #include "CollisionDetector.h"
 
-bool CollisionDetector::playerResourceCollision(const Player &player, const Scene &scene) const {
-    return false;
+void CollisionDetector::playerResourceCollision(const Player &player, const Scene &scene) {
+
+    playerX = player.getPlayerScreenPositionX();
+    playerY = player.getPlayerScreenPositionY();
+
+    playerRight = playerX + PLAYER_WIDTH;
+    playerBottom = playerY + PLAYER_HEIGHT;
+
+    blockHeight = SCREEN_HEIGHT/scene.getSceneRows();
+    blockWidth = SCREEN_WIDTH/scene.getSceneColumns();
+
+    playerUpperLeftResourceCollision(player, scene);
+    playerUpperRightResourceCollision(player, scene);
+    playerBottomLeftResourceCollision(player, scene);
+    playerBottomRightResourceCollision(player, scene);
+}
+
+void CollisionDetector::playerUpperLeftResourceCollision(const Player &player, const Scene &scene) const {
+
+    // find out which block the player's upper left corner is in
+    // int division truncates towards zero so this should work
+    
 }
